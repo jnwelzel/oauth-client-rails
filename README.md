4642dee49ae866bf94144698055cf992d8d234e9
Jon Welzel <jonathan.welzel@totvs.com.br>
2014-04-08 14:53:58 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#About
+
+Application created to act as an OAuth 1.0a client (consumer) for the [rest](https://github.com/jnwelzel/rest) application.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OauthClient::Application.config.secret_key_base = '63d7f1df14a36baae48df38b784be08470955f0a47ad763dcef00fc88061e10a756a82227753149eed9d918a26b7dcc670f1797681d809a5257a2b5e33224f69'
